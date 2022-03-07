e2315220fb8c78a706587b6cb3d2d7f4217923bb
Alimaa <alimaahashim@gmail.com>
2022-03-07 16:26:45 +0000

    add dark and light button

---
+++ app.js
@@ -0,0 +1,41 @@
+// darkModeButton = document.getElementById("dark-mode-button")
+
+// const originalBody = document.body.style.backgroundColor;
+
+// handleButtonClick = function () {
+//     if(originalBody === originalBody){
+//         console.log("button clicked")
+//     // change the background colour and text
+//     body = document.body
+//     body.style.backgroundColor = "#091d1e"
+//     body.style.color = "#aaaaaa"
+
+//     // change the border of the title
+
+//     title = document.getElementById("title-section")
+//     title.style.border = "1px solid #aaaaaa"
+
+//     // change the border and headings on the posts
+
+//     posts = document.getElementsByClassName("post")
+//     for (post of posts){
+//         post.style.border = "1px solid #aaaaaa"
+//     }
+
+//     postHeadings = document.getElementsByTagName("h3")
+//     for (heading of postHeadings){
+//         heading.style.color = "#466876"
+//     }
+// } else {
+//     originalBody = originalBody
+           
+// }
+    
+// }
+
+// darkModeButton.addEventListener("click", handleButtonClick)
+
+function darkModeButton(){
+    let element = document.body;
+    element.classList.toggle("dark-mode-button")
+} 
